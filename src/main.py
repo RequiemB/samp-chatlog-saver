74a06effb735d1b33eaddcccc2aafb9c202602be
RequiemB <irfansherif12@gmail.com>
2023-04-30 15:08:18 +0530

    Add double instance checker

--- src/main.py
+++ src/main.py
@@ -1,43 +1,36 @@
 import os
 import sys
 import logging
-import time
 
 from funcs.config import retrieve_configuration
-from funcs.saver import SAMPChatLogSaver, is_process_running
+from funcs.saver import SAMPChatLogSaver
 from funcs import logger 
 
+from win32event import CreateMutex
+from win32api import GetLastError
+from winerror import ERROR_ALREADY_EXISTS
+
 # Add a file handler
-file_handler = logging.FileHandler(os.path.join(os.path.dirname(__file__), "saver.log"))
+file_handler = logging.FileHandler("./saver.log")
 file_handler.setLevel(logging.INFO)
 file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_format)
 logger.addHandler(file_handler)
 
+# Thanks to https://www.oreilly.com/library/view/python-cookbook/0596001673/ch06s09.html
+handle = CreateMutex(None, 1, 'chatlog-saver.exe')
+
 # Check if the chatlog is already running
-def check_if_already_running() -> None:
-    if is_process_running('chatlog-saver.exe'):
-        value = input("Another instance of this chat-log saver is being run. Do you want to close it and initate this instance? (Y/N): ")
-        if value.lower() not in ['y', 'n']:
-            print("Invalid input.")
-            check_if_already_running()
-        if value.lower() == "y":
-            os.system("TASKKILL /im chatlog-saver.exe /f")
-            pass
-        else:
-            print("Quitting this instance...")
-            time.sleep(1)
-            sys.exit()
+if GetLastError() == ERROR_ALREADY_EXISTS:
+    logger.warning("Another instance of this program is already being run. Running it twice may save the chatlogs twice.")
 
 # Get the configuration
-config = retrieve_configuration(os.path.join(os.path.dirname(__file__), "config.json"))
-
+config = retrieve_configuration("./config.json")
 saver = SAMPChatLogSaver(config) # Initialize a SAMPChatLogSaver instance with the configuration
-if __name__ == '__main__':
-    try:
-        saver.run() # Run the program
-    except KeyboardInterrupt:
-        sys.exit()
+try:
+    saver.run() # Run the program
+except KeyboardInterrupt:
+    sys.exit()
 
 
 
